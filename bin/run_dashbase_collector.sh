f14a5ca52cf3c0abb0708af6be141316ce5d6da2
John Wang <john@dashbase.io>
2016-09-08 00:57:02 -0700

    fix cmdlien script

--- bin/run_dashbase_collector.sh
+++ bin/run_dashbase_collector.sh
@@ -12,18 +12,7 @@ logs=logs
 
 mkdir -p $logs
 
-if [[ -z $PORT ]]
-then
-  PORT=4567
-fi
-
-CONF_FILE=$1
-
-echo "port: $PORT"
-echo "configuration file loaded: $CONF_FILE"
-
 JAVA_OPTS="-Dlog4j.configuration=file://$(pwd)/conf/log4j.xml"
-CMD_ARGS="-s ${CONF_FILE} -p ${PORT}"
 
 #JAVA_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=y"
 #GC_OPTS="-XX:+UseConcMarkSweepGC -XX:+UseParNewGC"
@@ -31,4 +20,4 @@ CMD_ARGS="-s ${CONF_FILE} -p ${PORT}"
 MAIN_CLASS="io.dashbase.collector.DashbaseCollectorServer"
 CLASSPATH=$dist/*:$lib/*:$resources
 
-exec java $JAVA_OPTS $HEAP_OPTS $GC_OPTS $JMX_OPTS $JAVA_DEBUG -classpath $CLASSPATH -Dlog.home=$logs $MAIN_CLASS ${CMD_ARGS}
+exec java $JAVA_OPTS $HEAP_OPTS $GC_OPTS $JMX_OPTS $JAVA_DEBUG -classpath $CLASSPATH -Dlog.home=$logs $MAIN_CLASS $@
